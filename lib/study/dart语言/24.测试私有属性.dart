bcb96ede79bb83181fda966edea884e5793c56ab
yangguohai <yanggh@idoosmart.com>
2026-09-17 10:25:44 +0800

    配置macos 网络权限

--- lib/study/dart语言/24.测试私有属性.dart
+++ lib/study/dart语言/24.测试私有属性.dart
@@ -1,7 +1,7 @@
-import './类的私有属性.dart';
+// import './类的私有属性.dart';
 
 void main(List<String> args) {
-  Person p = Person.createPerson(name: '老高');
+  // Person p = Person.createPerson(name: '老高');
   // p.study();
   // p._study();
   // p._name = "";
